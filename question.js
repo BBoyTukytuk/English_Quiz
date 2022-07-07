98103d425f4fb4d0a931d5a7853b04cd46bf9d99
BBoyTukytuk <63485300+BBoyTukytuk@users.noreply.github.com>
2022-07-07 18:25:17 +1000

    Update question.js

--- question.js
+++ question.js
@@ -5,32 +5,39 @@ const quiz = [
 {
  q:'你别着急，一定会有办法的。',
  options:['Do not worry. There must be some solutions.','Do not worry about solutions.','Solutions are not to worry.','Do not worry. Where are the solutions.'],
- answer:0
+ answer:0,
+ audio_mp3: './audio/Only.mp3' 
 },
 {
   q:'最近发生了一件很奇怪的事。 ',
   options:['A stranger thing happened recently.','A strange thing happened recently.','A thing happened strange recently.','A strange things happen recently.'],
-  answer:1
+  answer:1,
+  audio_mp3: './audio/Only.mp3' 
  },
  {
   q:'Alex虽然不够聪明，但是很努力。',
   options:['Alex is clever, but he is not hard working.','Alex is not clever, but he is hard working.'],
-  answer:1
+  answer:1,
+  audio_mp3: './audio/Only.mp3' 
  },
  {
   q:'这个比赛对我来说很重要。',
   options:['This competition is too important for me.',' This competition was very important.', 'This competition is very important to me.'],
-  answer:2
+  answer:2,
+  audio_mp3: './audio/Only.mp3' 
  },
  {
   q:'要保持均衡饮食和适量的运动。',
   options:['Balance your diet and plenty of exercise.','Eat a balanced diet and with plenty exercise.', 'Eat some balanced diet and get plenty of exercise.','Eat a balanced diet and get plenty of exercise.'],
-  answer:3
+  answer:3,
+  audio_mp3: './audio/Only.mp3' 
  },
   {
   q:'你会如何描述这张图片呢？',
   options:['This is a wonderful family and I hope they will be together soon.', 'This is my wonderful family and I hope we will be together again soon.'],
   answer:1,
-  img:'https://user-images.githubusercontent.com/63485300/108956234-bfdb2b00-76b2-11eb-88e4-c5f629fea1b5.png' 
+  img:'https://user-images.githubusercontent.com/63485300/108956234-bfdb2b00-76b2-11eb-88e4-c5f629fea1b5.png',
+  alt:'This is a picture of your family smiling',
+  audio_mp3: './audio/Only.mp3' 
  },
 ]
